74605dd18558a33a6d5fce585d36097189d04978
Margokitaeva <147771338+Margokitaeva@users.noreply.github.com>
2025-07-19 09:43:23 +0200

    task1 done

---
+++ ishop0/App.js
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Shop from './components/Shop';
+
+ReactDOM.render(
+  <Shop shopName="Цветаева" shopAddress="ул. Настоящая д.19" />, 
+  document.getElementById('container') 
+);
---
+++ ishop0/components/Shop.js
@@ -0,0 +1,25 @@
+﻿import React from 'react';
+
+import './Shop.css';
+
+class Shop extends React.Component {
+
+  render() {
+    return (
+      <div className="Shop">
+          <p> Our shop is called {" "}
+              <span className="ShopName">
+                  {this.props.shopName}
+              </span>
+          </p>
+          <p>Our address: {" "}
+              <span className="ShopAddress">
+                  {this.props.shopAddress}
+              </span>
+          </p>
+      </div>
+    )
+  }
+}
+
+export default Shop;
